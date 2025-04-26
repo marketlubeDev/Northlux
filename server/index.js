a25b8d0cbce18e917b33b6ede8e864a73164f126
marketlubesDev <marketlubedevelopers@gmail.com>
2025-04-26 21:14:42 +0530

    cors updated

--- server/index.js
+++ server/index.js
@@ -19,7 +19,13 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: [
+      "https://northluxadmin.vercel.app",
+      "https://northluxuser.vercel.app",
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:5175",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
